91bd5eb7102ec9aa7fb5693b8452468c773e3e76
kaveenakash <kaveenakashya@gmail.com>
2021-01-08 04:59:47 +0530

    Layout Css module added

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import Auxi from '../../hoc/Auxi'
+import classes from './Layout.module.css'
 
 const layout = props =>{
     return(
         <Auxi>
             <div>Toolbar,Sidedrawer,Backdrop</div>
-            <main>
+            <main className={classes.Content}>
                 {props.children}
             </main>
         </Auxi>
