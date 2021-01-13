9f9a2437d57a142d97814d8468a11cc929baf328
kaveenakash <kaveenakashya@gmail.com>
2021-01-13 08:29:45 +0530

    Logo Added to the toolbar

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import Logo from '../../Logo/Logo'
 import classes from './Toolbar.module.css'
 
 const Toolbar = props =>{
     return(
         <header className={classes.Toolbar}>
             <div>Menu</div>
-            <div>LOGO</div>
+            <Logo/>
             <nav>
                 ..
             </nav>
