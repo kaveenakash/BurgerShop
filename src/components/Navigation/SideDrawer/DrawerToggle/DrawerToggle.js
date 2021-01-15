4eed863456e56580bbdd67b221554d857107fd22
kaveenakash <kaveenakashya@gmail.com>
2021-01-16 03:10:48 +0530

    Drawer Toggle Css Completed

--- src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
+++ src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
@@ -1,7 +1,13 @@
 import React from "react";
+import classes from './DrawerToggle.module.css'
 
 const DrawerToggle = (props) => {
-  return <div onClick={props.clicked}>Menu</div>;
+  return <div onClick={props.clicked} className={classes.DrawerToggle}>
+      <div></div>
+      <div></div>
+      <div></div>
+
+  </div>;
 };
 
 export default DrawerToggle;
